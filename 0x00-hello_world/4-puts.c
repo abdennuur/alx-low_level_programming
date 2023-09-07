34cdac98807881ccd7f85ebc8c4fc66cba84a555
abdennuur <abdnourkhatib@gmail.com>
2023-09-07 12:53:14 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
 {
-	/**
-	 *  main -Entry point
- 	*
-	 *  Return: Always 0 (Success)
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
